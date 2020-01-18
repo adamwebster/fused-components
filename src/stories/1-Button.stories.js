131b8e955dfe6b45fa9aee91e0a035a7f812cfa1
Adam Webster <adamwebster@me.com>
2020-01-18 17:15:18 -0500

    Added knobs

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 import AnimatedButton from '../components/ui/AnimatedButton';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withKnobs]
 };
 
-export const ButtonThatIsAnimated = () => <AnimatedButton completed>Hello Button</AnimatedButton>;
+export const ButtonThatIsAnimated = () => <AnimatedButton disabled={boolean("Disabled", false)} loading={boolean("Loading", false)} completed={boolean("Completed", false)}>Hello Button</AnimatedButton>;
 
