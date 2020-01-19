458817c58b095ba493f4f62d7df7ca83e9b12f74
Adam Webster <adamwebster@me.com>
2020-01-19 09:01:32 -0500

    Some updates to the story book stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,3 +2,6 @@ import { configure, addDecorator } from "@storybook/react"
 import themeDecorator from "./themeDecorator"
 
 addDecorator(themeDecorator);
+
+// automatically import all files ending in *.stories.js|mdx
+configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
