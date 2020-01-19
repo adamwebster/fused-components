6a1b44db9cde3d689aa393cf7a57f9138f969646
adamwebster <adamwebster@me.com>
2020-01-19 09:01:32 -0500

    Some updates to the story book stories

--- src/components/ui/AnimatedButton/index.jsx
+++ src/components/ui/AnimatedButton/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Button } from './style';
+import PropTypes from 'prop-types';
+
+/**
+- Use an avatar for attributing actions or content to specific users.
+- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
+**/
 
 const AnimatedButton = ({ disabled, primary, loading, completed, ...rest }) => {
   return (
@@ -14,5 +20,21 @@ const AnimatedButton = ({ disabled, primary, loading, completed, ...rest }) => {
     </>
   );
 };
+
+AnimatedButton.defaultProps ={
+  disabled: false,
+  completed: false,
+  loading: false,
+  primary:false,
+
+}
+
+AnimatedButton.propTypes = {
+  /** Sets if the button is disabled */
+  disabled: PropTypes.bool,
+  completed: PropTypes.bool,
+  loading: PropTypes.bool,
+  primary: PropTypes.bool,
+}
 
 export default AnimatedButton;
