e7b2ad4185a13e1a179e8252de0e7e5ed0e72248
Adam Webster <awebster@bnasdev.onmicrosoft.com>
2020-01-20 10:29:57 -0500

    Add styles

--- .storybook/themeDecorator.js
+++ .storybook/themeDecorator.js
@@ -1,10 +1,17 @@
 // themeDecorator.js
 import React from "react"
 import theme from "../src/themes"
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
 
+  body {
+    font: 13px/1.5 'Helvetica Neue', Arial, 'Liberation Sans', FreeSans, sans-serif;
+  }
+  `
 const ThemeDecorator = storyFn => (
-  <ThemeProvider theme={theme.default}>{storyFn()}</ThemeProvider>
+  <ThemeProvider theme={theme.default}>
+    <GlobalStyle />
+    {storyFn()}</ThemeProvider>
 )
 
 export default ThemeDecorator
