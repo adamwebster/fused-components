e98918b5b0c67e5f2cbee5b318bff2818282db28
Adam Webster <awebster@bnasdev.onmicrosoft.com>
2020-01-20 09:14:10 -0500

    Added label story

---
+++ src/components/ui/Label/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import { LabelStyled } from './style';
+
+const Label = ({...rest}) => {
+    return(
+        <LabelStyled {...rest} />
+    )
+}
+
+export default Label;
---
+++ src/components/ui/Label/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LabelStyled = styled.label`
+display:inline-block;
+color: #6d6d6d;
+margin-bottom:5px;
+`
