c0b12d7113b3656dfbf8f7a52157441b665fecf8
adamwebster <adamwebster@me.com>
2020-02-01 10:10:35 -0500

    Adding some props to tables

--- src/components/ui/Table/style.js
+++ src/components/ui/Table/style.js
@@ -5,26 +5,47 @@ export const StyledTable = styled.div`
   display: flex;
   flex: 1 1;
   flex-flow: column;
+    ${props => props.tableBGColor && css`
+        background-color: ${props.tableBGColor};
+    `}
+
+    ${props => props.tableTextColor && css`
+        color: ${props.tableTextColor};
+    `}
 `;
 
 export const TableHeader = styled.div`
   display: flex;
   flex: 1 1;
   border-bottom: solid 1px ${variables.borderColor};
+  ${props => props.tableHeaderBGColor && css`
+    background-color: ${props.tableHeaderBGColor};
+  `}
+  ${props => props.textColor && css`
+    color: ${props.textColor};
+  `}
 `;
 
 export const TableRow = styled.div`
   display: flex;
   flex: 1 1;
+  ${props => props.zebraStripping && css`
+    &:nth-child(odd){
+        background-color: ${props.zebraStripeColor};
+    }
+    `
+  }
 `;
 
 export const TableCell = styled.div`
   display: flex;
   flex: 1 1;
-${props => props.cellWidth && css`
-flex: unset;
-width: ${props.cellWidth};
-`};
+  ${props =>
+    props.cellWidth &&
+    css`
+      flex: unset;
+      width: ${props.cellWidth};
+    `};
   padding: ${props => props.cellPadding};
 `;
 
@@ -32,4 +53,10 @@ export const TableBody = styled.div`
   display: flex;
   flex: 1 1;
   flex-flow: column;
+  ${props => props.tableBodyBGColor && css`
+    background-color: ${props.tableBodyBGColor};
+  `}
+  ${props => props.tableBodyTextColor && css`
+    color: ${props.tableBodyTextColor};
+  `}
 `;
