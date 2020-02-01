1a014af17550cc49294c22bbd0417694fdd15736
Adam Webster <adamwebster@me.com>
2020-02-01 10:10:35 -0500

    Adding some props to tables

--- src/components/ui/Table/index.jsx
+++ src/components/ui/Table/index.jsx
@@ -8,11 +8,27 @@ import {
 } from "./style";
 import PropTypes from "prop-types";
 import SampleTableData from "../../../data/SampleTableData";
-export const Table = ({ cellPadding }) => {
+export const Table = ({
+  cellPadding,
+  tableHeaderBGColor,
+  tableHeaderTextColor,
+  zebraStripping,
+  zebraStripeColor,
+  tableBodyBGColor,
+  tableBodyTextColor,
+  tableBGColor,
+  tableTextColor,
+}) => {
   console.log(SampleTableData);
   return (
-    <StyledTable>
-      <TableHeader>
+    <StyledTable
+      tableTextColor={tableTextColor && tableTextColor}
+      tableBGColor={tableBGColor && tableBGColor}
+    >
+      <TableHeader
+        textColor={tableHeaderTextColor && tableHeaderTextColor}
+        tableHeaderBGColor={tableHeaderBGColor && tableHeaderBGColor}
+      >
         {SampleTableData.headers.map((header, index) => {
           console.log(header.width);
           return (
@@ -26,10 +42,17 @@ export const Table = ({ cellPadding }) => {
           );
         })}
       </TableHeader>
-      <TableBody>
+      <TableBody
+        tableBodyTextColor={tableBodyTextColor && tableBodyTextColor}
+        tableBodyBGColor={tableBodyBGColor && tableBodyBGColor}
+      >
         {SampleTableData.rows.map((row, index) => {
           return (
-            <TableRow key={index}>
+            <TableRow
+              zebraStripeColor={zebraStripeColor}
+              zebraStripping={zebraStripping}
+              key={index}
+            >
               {row.row.map((column, index) => {
                 return (
                   <TableCell
@@ -54,9 +77,19 @@ export const Table = ({ cellPadding }) => {
 
 Table.propTypes = {
   data: PropTypes.object,
-  cellPadding: PropTypes.string
+  cellPadding: PropTypes.string,
+  tableHeaderBGColor: PropTypes.string,
+  tableHeaderTextColor: PropTypes.string,
+  zebraStripping: PropTypes.bool,
+  zebraStripeColor: PropTypes.string,
+  tableBodyBGColor: PropTypes.string,
+  tableBodyTextColor: PropTypes.string,
+  tableBGColor: PropTypes.string,
+  tableTextColor: PropTypes.string,
 };
 
 Table.defaultProps = {
-  cellPadding: "5px"
+  cellPadding: "5px",
+  zebraStripping: false,
+  zebraStripeColor: "#ebebeb"
 };
