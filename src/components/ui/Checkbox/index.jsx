2c60086561ec110869ff9d6d34afbcddfb86836e
Adam Webster <adamwebster@me.com>
2020-02-02 19:59:11 -0500

    Added  properties

--- src/components/ui/Checkbox/index.jsx
+++ src/components/ui/Checkbox/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Label, Icon, CheckInput} from './style';
+import { Label, Icon, CheckInput } from "./style";
+import PropTypes from "prop-types";
 
 export const Checkbox = ({ children, checked }) => {
   return (
     <label>
       <CheckInput checked={checked} type="checkbox" />
-      {checked ? (
-        <Icon icon="check" />
-      ) : (
-        <Icon icon={["far", "square"]} />
-      )}
+      {checked ? <Icon icon="check" /> : <Icon icon={["far", "square"]} />}
       <Label>{children}</Label>
     </label>
   );
+};
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool
+};
+
+Checkbox.defaultProps = {
+  checked: false
 };
