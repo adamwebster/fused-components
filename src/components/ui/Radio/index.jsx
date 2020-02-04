4bb8d9956119cf652f4a795cd65929fd87235e65
adamwebster <adamwebster@me.com>
2020-02-03 19:53:49 -0500

    Added Error and Warning state for checkboxes and radios

--- src/components/ui/Radio/index.jsx
+++ src/components/ui/Radio/index.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Label, Icon, RadioInput } from "./style";
 import PropTypes from "prop-types";
 
-export const Radio = ({ children, checked, ...radioProps }) => {
+export const Radio = ({ children, checked, inWarning, inError, ...radioProps }) => {
   return (
     <label>
-      <RadioInput type="radio" {...radioProps} />
-      {checked ? <Icon transform="shrink-10" icon={["fas", "circle"]} mask={['fas', 'circle']}/> : <Icon icon={["far", "circle"]} />}
-      <Label>{children}</Label>
+      <RadioInput inError={inError} inWarning={inWarning} type="radio" {...radioProps} />
+      {checked ? <Icon inError={inError} inWarning={inWarning} transform="shrink-10" icon={["fas", "circle"]} mask={['fas', 'circle']}/> : <Icon inError={inError} inWarning={inWarning} icon={["far", "circle"]} />}
+      <Label inError={inError} inWarning={inWarning}>{children}</Label>
     </label>
   );
 };
 
 Radio.propTypes = {
-  checked: PropTypes.bool
+  checked: PropTypes.bool,
+  inError: PropTypes.bool,
+  inWarning: PropTypes.bool,
 };
 
 Radio.defaultProps = {
-  checked: false
+  checked: false,
+  inError: false,
+  inWarning: false,
 };
--- src/components/ui/Radio/style.js
+++ src/components/ui/Radio/style.js
@@ -1,15 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { color } from '../../../styles/styles';
 
 export const Label = styled.span`
     margin-left: 5px;
     display:inline-block;
+    ${props => props.inError && css`
+        color: ${color.red};
+    `}
+    ${props => props.inWarning && css`
+        color: ${color.yellow};
+    `}
 `
 
 export const Icon = styled(FontAwesomeIcon)`
     color: ${color.primary};
     font-size: 15px;
+    ${props => props.inError && css`
+        color: ${color.red};
+    `}
+    ${props => props.inWarning && css`
+        color: ${color.yellow};
+    `}
 `
 
 export const RadioInput = styled.input`
