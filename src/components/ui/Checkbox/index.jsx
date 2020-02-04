c3c806a0d26f7280b936303a35b1b5b0c7ebf38d
Adam Webster <adamwebster@me.com>
2020-02-03 19:53:49 -0500

    Added Error and Warning state for checkboxes and radios

--- src/components/ui/Checkbox/index.jsx
+++ src/components/ui/Checkbox/index.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Label, Icon, CheckInput } from "./style";
 import PropTypes from "prop-types";
 
-export const Checkbox = ({ children, checked, ...checkboxProps }) => {
+export const Checkbox = ({ children, checked, inError, inWarning, ...checkboxProps }) => {
   return (
     <label>
       <CheckInput checked={checked} type="checkbox" {...checkboxProps} />
-      {checked ? <Icon transform="shrink-6" icon="check" mask={['fas', 'square']}/> : <Icon icon={["far", "square"]} />}
-      <Label>{children}</Label>
+      {checked ? <Icon  inError={inError} inWarning={inWarning} transform="shrink-6" icon="check" mask={['fas', 'square']}/> : <Icon inError={inError} inWarning={inWarning} icon={["far", "square"]} />}
+      <Label inError={inError} inWarning={inWarning}>{children}</Label>
     </label>
   );
 };
 
 Checkbox.propTypes = {
-  checked: PropTypes.bool
+  checked: PropTypes.bool,
+  inError: PropTypes.bool,
+  inWarning: PropTypes.bool
 };
 
 Checkbox.defaultProps = {
-  checked: false
+  checked: false,
+  inError: false,
+  inWarning: false,
 };
