54f036d8f92c18d25bf2789611e48db24859e96d
adamwebster <adamwebster@me.com>
2020-02-15 13:39:36 -0500

    Updating corner dialog example

--- src/demos/CornerDialogDemo.jsx
+++ src/demos/CornerDialogDemo.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { CornerDialog } from "../components/ui/CornerDialog";
+import { Button } from "../components/ui/Button";
 
 export const CornerDialogDemo = () => {
-  const [dialogVisible, setDialogVisible] = useState(true);
+  const [dialogVisible, setDialogVisible] = useState(false);
   return (
+      <>
+      <Button onClick={() => setDialogVisible(true)} disabled={dialogVisible}>Show Corner Dialog</Button>
     <CornerDialog
       onCloseClick={() => setDialogVisible(false)}
       visible={dialogVisible}
@@ -11,5 +14,6 @@ export const CornerDialogDemo = () => {
     >
       Click the close button above or below to hide the corner dialog.
     </CornerDialog>
+    </>
   );
 };
