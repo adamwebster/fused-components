533fd042b5b55ff4015cd8332686de158331bfdc
Adam Webster <adamwebster@me.com>
2020-02-16 11:45:09 -0500

    Working on dropdown button

--- src/components/ui/DropdownButton/MenuItem.jsx
+++ src/components/ui/DropdownButton/MenuItem.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import { MenuItemStyled } from "./style";
+import Icon from "../../icon";
 
-export const MenuItem = ({ children }) => {
-  return <MenuItemStyled>{children} </MenuItemStyled>;
+export const MenuItem = ({ children, icon, ...rest }) => {
+  return <MenuItemStyled {...rest}>
+  {icon && <Icon icon={icon} />}
+  {children} </MenuItemStyled>;
 };
--- src/components/ui/DropdownButton/index.jsx
+++ src/components/ui/DropdownButton/index.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import { Button } from "../Button";
 
-import { DropdownButtonWrapper , IconStyled } from "./style";
+import { DropdownButtonWrapper , IconStyled, MenuDivider } from "./style";
 import { DropdownMenu } from "./DropdownMenu";
 import { MenuItem } from "./MenuItem";
 import DropdownMenuContext from "./DropdownMenuContext";
 import Icon from '../../icon';
 
-export const DropdownButton = ({ primary, label, children }) => {
+export const DropdownButton = ({ primary, hasIcons, label, fcStyle, children }) => {
   const [menuOpen, setMenuOpen] = useState(false);
     const [menuVisible, setMenuVisible] = useState(false);
 
     const state = {
-        menuOpen
+        menuOpen,
+        hasIcons,
       };
-
+    
   const toggleMenu= () => {
     if(menuOpen){
         setMenuOpen(false);
         setTimeout(() => {
             setMenuVisible(false);
-        }, 500)
+        }, 200)
      }else{
          setMenuVisible(true);
         setMenuOpen(true);
@@ -30,7 +31,7 @@ export const DropdownButton = ({ primary, label, children }) => {
   return (
     <DropdownButtonWrapper>
       <DropdownMenuContext.Provider value={state}>
-        <Button primary={primary} onClick={() => toggleMenu()}>
+        <Button fcStyle={fcStyle} primary={primary} onClick={() => toggleMenu()}>
           {label}
           {menuOpen ? <IconStyled><Icon icon='caret-up'/></IconStyled> :<IconStyled><Icon icon='caret-down'/></IconStyled>}
         </Button>
@@ -42,3 +43,4 @@ export const DropdownButton = ({ primary, label, children }) => {
 
 DropdownButton.Menu = DropdownMenu;
 DropdownButton.MenuItem = MenuItem;
+DropdownButton.Divider = MenuDivider;
