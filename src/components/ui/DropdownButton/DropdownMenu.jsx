234ea8e8639b2bee76c82587ef6a38eb83161a04
adamwebster <adamwebster@me.com>
2020-02-16 12:25:59 -0500

    Working on hidig menu when it is clicked outside of the menu

--- src/components/ui/DropdownButton/DropdownMenu.jsx
+++ src/components/ui/DropdownButton/DropdownMenu.jsx
@@ -1,14 +1,32 @@
-import React  from "react";
+import React, {useContext, useEffect, useRef}  from "react";
 
 import { DropdownMenuStyled } from "./style";
 import DropdownMenuContext from "./DropdownMenuContext";
 
-export const DropdownMenu = ({ children, context, props }) => {
+export const DropdownMenu = ({ children }) => {
+  const DropdownContext = useContext(DropdownMenuContext);
+const menuRef = useRef(null);
+  const handleClickOutside = (e) => {
+    console.log('here', menuRef, e.target.parentNode)
+    console.log(menuRef.current === e.target.parentNode)
+    if(menuRef.current !== e.target.parentNode){
+      DropdownContext.hideMenu();
+    }
+  }
+  useEffect(() => {
+  //  DropdownContext.hideMenu();
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  })
   return (
     <DropdownMenuContext.Consumer>
       {({ menuOpen, hasIcons}) => (
         <>
           <DropdownMenuStyled
+          ref={menuRef}
             hasIcons={hasIcons}
             menuOpen={menuOpen}
           >
--- src/components/ui/DropdownButton/index.jsx
+++ src/components/ui/DropdownButton/index.jsx
@@ -1,39 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "../Button";
 
-import { DropdownButtonWrapper , IconStyled, MenuDivider } from "./style";
+import { DropdownButtonWrapper, IconStyled, MenuDivider } from "./style";
 import { DropdownMenu } from "./DropdownMenu";
 import { MenuItem } from "./MenuItem";
 import DropdownMenuContext from "./DropdownMenuContext";
-import Icon from '../../icon';
+import Icon from "../../icon";
 
-export const DropdownButton = ({ primary, hasIcons, label, fcStyle, children }) => {
+var ref = null;
+export const DropdownButton = ({
+  primary,
+  hasIcons,
+  label,
+  fcStyle,
+  children
+}) => {
   const [menuOpen, setMenuOpen] = useState(false);
-    const [menuVisible, setMenuVisible] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+  const menuEl = useRef(null);
 
-    const state = {
-        menuOpen,
-        hasIcons,
-      };
-    
-  const toggleMenu= () => {
-    if(menuOpen){
-        setMenuOpen(false);
-        setTimeout(() => {
-            setMenuVisible(false);
-        }, 200)
-     }else{
-         setMenuVisible(true);
-        setMenuOpen(true);
-     }
+  const hideMenuFunc = () => {
+    setMenuVisible(false);
+    setMenuOpen(false);
+
+    console.log("hideMenu");
+  };
+  const state = {
+    menuOpen,
+    hasIcons,
+    hideMenu: () => hideMenuFunc()
+  };
+
+  const toggleMenu = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+      setTimeout(() => {
+        setMenuVisible(false);
+      }, 200);
+    } else {
+      setMenuVisible(true);
+      setMenuOpen(true);
     }
+  };
 
   return (
     <DropdownButtonWrapper>
       <DropdownMenuContext.Provider value={state}>
-        <Button fcStyle={fcStyle} primary={primary} onClick={() => toggleMenu()}>
+        <Button
+          fcStyle={fcStyle}
+          primary={primary}
+          onClick={() => toggleMenu()}
+        >
           {label}
-          {menuOpen ? <IconStyled><Icon icon='caret-up'/></IconStyled> :<IconStyled><Icon icon='caret-down'/></IconStyled>}
+          {menuOpen ? (
+            <IconStyled>
+              <Icon icon="caret-up" />
+            </IconStyled>
+          ) : (
+            <IconStyled>
+              <Icon icon="caret-down" />
+            </IconStyled>
+          )}
         </Button>
         {menuVisible && children}
       </DropdownMenuContext.Provider>
