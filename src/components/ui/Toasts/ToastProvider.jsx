ce09adbbe0cbdc4cab53383ea17e13b454236b8a
adamwebster <adamwebster@me.com>
2020-02-17 19:58:21 -0500

    Toasts

---
+++ src/components/ui/Toasts/ToastProvider.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useContext, useEffect } from "react";
+import ToastContext from "./ToastContext";
+import {ToastContainer, StyledToast} from './styles';
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  useEffect(() => {
+    if(toasts.length > 0){
+        setTimeout(() => {
+            
+            let toastsToSet = toasts.slice(-1, 1);
+            if(toasts.length === 1){
+                toastsToSet = [];
+            }
+          setToasts(toastsToSet);
+        }, 5000)
+    }
+  },[toasts])
+  const add = (title, content, fcStyle) => {
+    const toAdd = toasts.slice();
+    toAdd.unshift({ title, content, fcStyle });
+    console.log(toAdd)
+    setToasts(toAdd);
+  };
+
+  const state = {
+    add: (title, content, style) => add(title, content, style),
+    test: "test"
+  };
+  return (
+    <ToastContext.Provider value={state}>
+      {children}
+      <ToastContainer id="Toasts">
+        {toasts.map((toast) => {
+          return <StyledToast
+            fcStyle={toast.fcStyle && toast.fcStyle}
+            title={toast.title && toast.title}
+          >{toast.content && toast.content}
+          </StyledToast>
+        })}
+      </ToastContainer>
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  return {
+    add: (title, content, style) => context.add(title, content, style)
+  };
+};
