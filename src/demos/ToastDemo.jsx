23d08952884e2716eb7efe21a36140877668a486
adamwebster <adamwebster@me.com>
2020-02-18 16:58:46 -0500

    Updatting toasts story

--- src/demos/ToastDemo.jsx
+++ src/demos/ToastDemo.jsx
@@ -1,40 +1,74 @@
-import React, {useState} from 'react';
-import { useToast, ToastProvider } from '../components/ui/Toasts/ToastProvider';
-import { Button } from '../components/ui/Button';
+import React, { useState } from "react";
+import { useToast, ToastProvider } from "../components/ui/Toasts/ToastProvider";
+import { Button } from "../components/ui/Button";
 
 const Toaster = () => {
-    const toasts = useToast();
-    const [count, setCount] = useState(0)
+  const toasts = useToast();
 
-    return(
-        <>
-        <Button onClick={() => {setCount(count + 1); toasts.addInfo('Did you know?', 'That space smells like seared steak.')}}>Info Toast</Button>
-  <br/> <br/>
-        <Button onClick={() => {setCount(count + 1); toasts.addSuccess('Hooray!', 'Something went right for once...', {duration: 10})}}>Success Toast (Duration set to 10sec)</Button>
-        <br/> <br/>
-        <Button onClick={() => {setCount(count + 1); toasts.addWarning('Warning', 'Winter is coming.', {id: 'one'})}}>Warning Toast (Maximum 1)</Button>
-        <br/> <br/>
-        <Button onClick={() => {setCount(count + 1); toasts.addDanger('Ok fly boy', 'Highway to the danger zone')}}>Danger Toast</Button>
-        <br/> <br/>
-        <Button onClick={() => {setCount(count + 1); toasts.addDanger('Danger Will Robinson')}}>Danger Toast No Content</Button>
-
-        </>
-        )
-}
+  return (
+    <>
+      <Button
+        onClick={() => {
+          toasts.addInfo(
+            "Did you know?",
+            "That space smells like seared steak."
+          );
+        }}
+      >
+        Info Toast
+      </Button>
+      <br /> <br />
+      <Button
+        onClick={() => {
+          toasts.addSuccess("Hooray!", "Something went right for once...", {
+            duration: 10
+          });
+        }}
+      >
+        Success Toast (Duration set to 10sec)
+      </Button>
+      <br /> <br />
+      <Button
+        onClick={() => {
+          toasts.addWarning("Warning", "Winter is coming.", { id: "one" });
+        }}
+      >
+        Warning Toast (Maximum 1)
+      </Button>
+      <br /> <br />
+      <Button
+        onClick={() => {
+          toasts.addDanger("Ok fly boy", "Highway to the danger zone");
+        }}
+      >
+        Danger Toast
+      </Button>
+      <br /> <br />
+      <Button
+        onClick={() => {
+          toasts.addDanger("Danger Will Robinson");
+        }}
+      >
+        Danger Toast No Content
+      </Button>
+    </>
+  );
+};
 export const ToastDemo = () => {
-    const [position, setPosition] =useState('top');
-    
-    return(
-        <ToastProvider position={position}>
-            <label>Position</label>
-            <select onChange={(e) => setPosition(e.target.value)}>
-                <option value="top">top</option>
-                <option value="bottom">bottom</option>
-                <option value="bottom-left">top-right</option>
-                <option value="bottom-right">bottom-right</option>
-            </select>
-            <br/><br/>
-            <Toaster />
-        </ToastProvider>
-    )
-}
+  const [position, setPosition] = useState("top");
+
+  return (
+    <ToastProvider position={position}>
+      <label>Position</label>
+      <select onChange={e => setPosition(e.target.value)}>
+        <option value="top">top</option>
+        <option value="bottom">bottom</option>
+        <option value="bottom-left">top-right</option>
+        <option value="bottom-right">bottom-right</option>
+      </select>
+      <br />
+      <br />
+      <Toaster />
+    </ToastProvider>
+  );
+};
