400a083721943d4d191a51df0c01c3cbe3839971
adamwebster <adamwebster@me.com>
2020-02-23 16:32:34 -0500

    Toggle dark mode

--- src/demos/ToggleDemo.tsx
+++ src/demos/ToggleDemo.tsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
-import { Toggle } from '../components/ui/Toggle'
+import React, { useState } from "react";
+import { Toggle } from "../components/ui/Toggle";
+import { FCThemeProvider } from "../theming/FCTheme";
 
 export const ToggleDemo = () => {
   const [active, setActive] = useState(false);
   const [activeLabels, setActiveLabels] = useState(false);
-
+  const [theme, setTheme] = useState('light');
   return (
     <>
-      <Toggle active={active} onClick={() => setActive(!active)} />
-      <br />
-      <Toggle active={activeLabels} showLabels onClick={() => setActiveLabels(!activeLabels)} />
+      <FCThemeProvider value={{ theme }}>
+        <p>
+      <label>Theme </label>
+      <select onChange={e => setTheme(e.target.value)}>
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+      </select>
+      </p>
+        <Toggle active={active} onClick={() => setActive(!active)} />
+        <br />
+        <Toggle
+          active={activeLabels}
+          showLabels
+          onClick={() => setActiveLabels(!activeLabels)}
+        />
+      </FCThemeProvider>
     </>
-  )
-}
+  );
+};
