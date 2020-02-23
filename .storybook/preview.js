498c7c841091d8cdae32d4c6c19cb385299ab66c
adamwebster <adamwebster@me.com>
2020-02-23 11:59:44 -0500

    Started work on themeing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,36 +1,28 @@
-import React from 'react';
-import { addDecorator, addParameters } from '@storybook/react';
+import React from "react";
+import { addDecorator, addParameters } from "@storybook/react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
-import { DocsPage } from 'storybook-addon-deps/blocks';
-import { withA11y } from '@storybook/addon-a11y';
+import { DocsPage } from "storybook-addon-deps/blocks";
+import { withA11y } from "@storybook/addon-a11y";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FCThemeProvider } from "../src/theming/FCTheme";
 library.add(fab, fas, far);
 
 addParameters({
   options: {
-    showRoots: true,
+    showRoots: true
   },
   docs: { page: DocsPage },
   dependencies: { withStoriesOnly: true, hideEmpty: false }
 });
 
-addDecorator(withA11y)
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font: 13px/1.5 'Helvetica Neue', Arial, 'Liberation Sans', FreeSans, sans-serif;
-  }
-  `
+addDecorator(withA11y);
 
 addDecorator(story => (
   <>
-    <GlobalStyle />
-    {story()}
+   {story()}
   </>
 ));
-
