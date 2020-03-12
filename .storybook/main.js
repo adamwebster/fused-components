5979f366c6374235cd48ac32c60eaa17506caf44
Adam Webster <adamwebster@me.com>
2020-03-11 22:13:28 -0400

    Running version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,51 +1,57 @@
 const path = require("path");
 
-const excludeProps = (prop) => {
+const excludeProps = prop => {
   if (prop.parent) {
-    return !prop.parent.fileName.includes('node_modules')
+    return !prop.parent.fileName.includes("node_modules");
   }
-  return true
-}
+  return true;
+};
 module.exports = {
-  presets: [{
-    name: 'storybook-addon-deps/preset',
-    options: {
-      exclude:  /^@babel/
-    }
-  }],
-  addons: [
+  presets: [
     {
-    name: '@storybook/preset-typescript',
-    options: {
-      tsDocgenLoaderOptions: {
-        tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
-        propFilter: props => excludeProps(props),
+      name: "storybook-addon-deps/preset",
+      options: {
+        exclude: /^@babel/
       }
     }
-    },
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-docs/preset',
-    '@storybook/addon-viewport/register',
-    '@storybook/addon-a11y/register',
-    'storybook-addon-deps'
   ],
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
+  addons: [
+    {
+      name: "@storybook/preset-typescript",
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, "../tsconfig.json")
+        },
+        forkTsCheckerWebpackPluginOptions: {
+          colors: false // disables built-in colors in logger messages
+        },
+        include: [path.resolve(__dirname, "../src")],
+        transpileManager: true
+      }
+    },
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-docs/preset",
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-a11y/register",
+    "storybook-addon-deps"
+  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.js"],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
+        // {
+        //   loader: require.resolve("ts-loader")
+        // },
         // Optional
         {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
+          loader: require.resolve("react-docgen-typescript-loader")
+        }
+      ]
     });
-    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.extensions.push(".ts", ".tsx");
     return config;
-  },
+  }
 };
