49b7a7d9befe38302e772fe1e5bbfc7b96814a05
Adam Webster <adamwebster@me.com>
2020-04-20 16:24:14 -0400

    Updated textarea component

--- src/components/ui/Textarea/styles.ts
+++ src/components/ui/Textarea/styles.ts
@@ -5,6 +5,15 @@ export const StyledTextarea = styled.textarea`
   border-radius: 5px;
   color: inherit;
   padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  resize: vertical;
+  outline: none;
   border: solid 1px ${(props): string => (props.theme === 'dark' ? color.darkModeMedium : color.border)};
   background-color: ${(props): string => (props.theme === 'dark' ? color.darkModeDarker : '#fff')};
+  &:focus {
+    border-color: ${(props): string => (props.theme === 'dark' ? color.darkModeMedium : color.focus)};
+    box-shadow: 0 0 6px ${(props): string => (props.theme === 'dark' ? color.darkModeMedium : color.focus)};
+  }
 `;
--- src/components/ui/Textarea/textarea.tsx
+++ src/components/ui/Textarea/textarea.tsx
@@ -2,8 +2,10 @@ import React, { useContext, ReactElement } from 'react';
 import { StyledTextarea } from './styles';
 import { FCTheme } from '../../../theming/FCTheme';
 
-export const Textarea = ({ ...rest }): ReactElement => {
+const Textarea = ({ ...rest }): ReactElement => {
   const theme = useContext(FCTheme);
 
   return <StyledTextarea theme={theme?.theme} {...rest} />;
 };
+
+export default Textarea;
