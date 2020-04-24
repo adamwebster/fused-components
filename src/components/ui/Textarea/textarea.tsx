6b47fb3d02ea0466ddb884f324c811fa052e585c
Adam Webster <adamwebster@me.com>
2020-04-24 16:19:47 -0400

    Removing memo

--- src/components/ui/Textarea/textarea.tsx
+++ src/components/ui/Textarea/textarea.tsx
@@ -2,9 +2,9 @@ import React, { useContext, ReactElement, TextareaHTMLAttributes } from 'react';
 import { StyledTextarea } from './styles';
 import { FCTheme } from '../../../theming/FCTheme';
 
-const Textarea = React.memo(function Textarea({ ...rest }: TextareaHTMLAttributes<HTMLTextAreaElement>): ReactElement {
+const Textarea = ({ ...rest }: TextareaHTMLAttributes<HTMLTextAreaElement>): ReactElement => {
   const theme = useContext(FCTheme);
   return <StyledTextarea theme={theme?.theme} {...rest} />;
-});
+};
 
 export default Textarea;
