418f5ffe3eb96221dd694aefad54c15c96e030bd
Adam Webster <adamwebster@me.com>
2020-04-23 22:02:35 -0400

    Adding memo to some components

--- src/components/ui/Label/index.tsx
+++ src/components/ui/Label/index.tsx
@@ -1,9 +1,18 @@
-import React, { useContext, ReactElement } from 'react';
+import React, { useContext, ReactNode, ReactElement } from 'react';
 
 import { LabelStyled } from './style';
 import { FCTheme } from '../../../theming/FCTheme';
 
-export const Label = ({ ...rest }): ReactElement => {
+interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: ReactNode;
+}
+export const Label = React.memo<Props>(function Label({ children, ...rest }: Props): ReactElement {
   const theme = useContext(FCTheme);
-  return <LabelStyled theme={theme?.theme} {...rest} />;
-};
+  return (
+    <LabelStyled theme={theme?.theme} {...rest}>
+      {children}
+    </LabelStyled>
+  );
+});
+
+Label.displayName = 'Label';
