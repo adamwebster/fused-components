f110d26a3f5924cd3c387598529844716a57a4cd
Adam Webster <adamwebster@me.com>
2020-04-26 20:38:52 -0400

    Attach to another div with an id

--- src/components/ui/Tooltip/tooltip.tsx
+++ src/components/ui/Tooltip/tooltip.tsx
@@ -6,41 +6,58 @@ interface TooltipPopoverProps {
   parentRef: any;
   content: ReactNode;
   toolTipVisible: boolean;
+  targetElement: HTMLElement | null;
 }
-const TooltipPopover = ({ parentRef, content, toolTipVisible }: TooltipPopoverProps): ReactElement => {
+const TooltipPopover = ({ parentRef, content, targetElement, toolTipVisible }: TooltipPopoverProps): ReactElement => {
   const ref = useRef<HTMLDivElement>(null);
   const [self, setSelf] = useState<any>(null);
-
   useEffect(() => {
     setSelf(ref.current);
     console.log(parentRef.offsetWidth);
   }, [toolTipVisible]);
 
-  if (toolTipVisible)
+  if (targetElement && toolTipVisible) {
     return ReactDOM.createPortal(
+      <StyledTooltip
+        ref={ref}
+        leftPosition={self ? targetElement.offsetWidth / 2 - self.offsetWidth / 2 : 0}
+        divHeight={self ? self.offsetHeight : 0}
+      >
+        {content}
+      </StyledTooltip>,
+      targetElement,
+    );
+  }
+  if (toolTipVisible)
+    return (
       <StyledTooltip
         ref={ref}
         leftPosition={self ? parentRef.offsetWidth / 2 - self.offsetWidth / 2 : 0}
         divHeight={self ? self.offsetHeight : 0}
       >
         {content}
-      </StyledTooltip>,
-      parentRef,
+      </StyledTooltip>
     );
   return <></>;
 };
 interface Props {
   children?: ReactNode;
   content?: ReactNode;
+  targetElementId: string;
 }
 
-const Tooltip = ({ children, content }: Props): ReactElement => {
+const Tooltip = ({ children, content, targetElementId = '' }: Props): ReactElement => {
   const [parentNode, setParentNode] = useState<any>(document.body);
   const [toolTipVisible, setToolTipVisible] = useState(false);
+  const [target, setTarget] = useState<any>(null);
+
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setParentNode(ref.current);
+    if (targetElementId) {
+      setTarget(document.getElementById(targetElementId));
+    }
   }, []);
 
   const showTooltip = (): void => {
@@ -51,7 +68,7 @@ const Tooltip = ({ children, content }: Props): ReactElement => {
   };
   return (
     <ToolTipWrapper onMouseOver={(): void => showTooltip()} onMouseOut={(): void => hideTooltip()} ref={ref}>
-      <TooltipPopover toolTipVisible={toolTipVisible} content={content} parentRef={parentNode} />
+      <TooltipPopover targetElement={target} toolTipVisible={toolTipVisible} content={content} parentRef={parentNode} />
       {children}
     </ToolTipWrapper>
   );
