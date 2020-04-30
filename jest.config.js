7f407ebb504157f5ed824b692a6aa94514bdcd7b
Adam Webster <adamwebster@me.com>
2020-04-30 19:14:37 -0400

    Added coverageThreshold to the jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // sets ut test files
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
