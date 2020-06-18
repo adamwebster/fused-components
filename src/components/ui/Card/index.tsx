33d7f3bfd01435c3f7f23cbe5f4a6f05432b268c
adamwebster <adamwebster@me.com>
2020-06-18 07:45:52 -0400

    Add as property to card

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, ReactElement, ReactNode } from 'react';
 import { StyledCard } from './style';
 import { FCTheme } from '../../../theming/FCTheme';
-export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+export interface Props extends React.HTMLAttributes<HTMLElement> {
   /** Sets the border radius for the card */
   borderRadius?: string;
   /** Sets the card to have a box shadow */
@@ -10,11 +10,22 @@ export interface Props extends React.HTMLAttributes<HTMLDivElement> {
    * @ignore
    */
   children?: ReactNode;
+  /**
+   * @ignore
+   */
+  as?: React.ElementType;
 }
-export const Card = ({ borderRadius = '5px', boxShadow = false, children, ...rest }: Props): ReactElement => {
+export const Card = ({
+  borderRadius = '5px',
+  boxShadow = false,
+  children,
+  as = 'div',
+  ...rest
+}: Props): ReactElement => {
   const theme = useContext(FCTheme);
+
   return (
-    <StyledCard theme={theme.theme} borderRadius={borderRadius} boxShadow={boxShadow} {...rest}>
+    <StyledCard as={as} theme={theme.theme} borderRadius={borderRadius} boxShadow={boxShadow} {...rest}>
       {children}
     </StyledCard>
   );
