8cb47ee59b6c5004ede2164e32ce030e8eba7176
adamwebster <adamwebster@me.com>
2021-01-20 08:28:34 -0500

    Removing vertical navigation component

--- src/components/ui/VerticalNavigation/VerticalNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-
-const VerticalNavigation = (): ReactElement => {
-  return <>Vertical Nav</>;
-};
-
-VerticalNavigation.displayName = 'VerticalNavigation';
-
-export default VerticalNavigation;
